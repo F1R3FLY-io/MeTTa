0411aaa7672a8e0b13415e6ebd60408272273c4f
Mike Stay <stay@pyrofex.net>
2025-03-18 16:05:40 -0600

    Add check for free vars in RHS of rewrite

--- GSLT/src/main/haskell/Module2CF/Check.hs
+++ GSLT/src/main/haskell/Module2CF/Check.hs
@@ -55,6 +55,50 @@ checkProduction prodName items =
   in go items 0
 
 --------------------------------------------------------------------------------
+-- * Rewrite Checking
+--------------------------------------------------------------------------------
+
+-- | Collects all variable names (ASTVar) from an AST expression.
+collectVars :: AST -> [Ident]
+collectVars (ASTVar ident) = [ident]
+collectVars (ASTSubst a1 a2 ident) = collectVars a1 ++ collectVars a2 ++ [ident]
+collectVars (ASTSExp _ asts) = concatMap collectVars asts
+
+-- | Extracts variable identifiers from a hypothesis.
+collectHypVars :: Hypothesis -> [Ident]
+collectHypVars (Hyp ident1 ident2) = [ident1, ident2]
+
+-- | Checks a RewriteBase rule ensuring all right-hand side variables appear on the left.
+checkRewriteBase :: String -> AST -> AST -> Either String ()
+checkRewriteBase ruleName lhs rhs =
+  let lhsVars = collectVars lhs
+      rhsVars = collectVars rhs
+      missing = filter (`notElem` lhsVars) rhsVars
+  in if null missing
+     then Right ()
+     else Left $ "Rewrite rule '" ++ ruleName ++ "' contains undefined variables on the right-hand side: " ++ show missing
+
+-- | Checks a rewrite rule, incorporating hypotheses when present.
+checkRewrite :: RewriteDecl -> Either String ()
+checkRewrite (RDecl (Ident ruleName) rewrite) = case rewrite of
+  RewriteBase lhs rhs -> checkRewriteBase ruleName lhs rhs
+  RewriteContext (Hyp ident1 ident2) innerRewrite ->
+    let extraVars = [ident1, ident2]  -- Include hypothesis variables
+    in case innerRewrite of
+         RewriteBase lhs rhs ->
+           let lhsVars = collectVars lhs ++ extraVars
+               rhsVars = collectVars rhs
+               missing = filter (`notElem` lhsVars) rhsVars
+           in if null missing
+              then Right ()
+              else Left $ "Rewrite rule '" ++ ruleName ++ "' (with hypothesis) contains undefined variables on the right-hand side: " ++ show missing
+         _ -> Left "Unexpected nested rewrite structure."
+
+-- | Checks all rewrites in a declaration.
+checkRewrites :: Rewrites -> Either String ()
+checkRewrites (Transitions rewrites) = mapM_ checkRewrite rewrites
+
+--------------------------------------------------------------------------------
 -- * Module and Declaration Checking
 --------------------------------------------------------------------------------
 
@@ -70,5 +114,7 @@ checkProg _            = Right ()
 -- | Checks a declaration.
 -- For GSLTDeclAll declarations, it checks the associated grammar.
 checkDecl :: Decl -> Either String ()
-checkDecl (GSLTDeclAll _ _ _ (Generators g) _ _) = checkGrammar g
-checkDecl _                                      = Right ()
+checkDecl (GSLTDeclAll _ _ _ (Generators g) _ rewrites) = do
+  checkGrammar g
+  checkRewrites rewrites
+checkDecl _ = Right ()

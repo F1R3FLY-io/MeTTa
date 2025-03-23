42c6254f9c118b43633baf78d9de4e5d153027fa
Mike Stay <metaweta@gmail.com>
2025-03-22 19:47:44 -0600

    Add module resolution

--- MeTTaIL/src/main/scala/io/f1r3fly/mettail/Main.scala
+++ MeTTaIL/src/main/scala/io/f1r3fly/mettail/Main.scala
@@ -1,14 +1,14 @@
 package io.f1r3fly.mettail
 
-import java.io.{FileReader, InputStreamReader, Reader}
+import java.io.{File, FileReader, InputStreamReader, Reader}
 import metta_venus.{MettaVenusLexer, MettaVenusParser, PrettyPrinter}
+import metta_venus.Absyn.{Module, Import, ImportModule, ImportModuleAs, ImportFromModule}
 import org.antlr.v4.runtime.{ANTLRInputStream, CommonTokenStream, ANTLRErrorListener, RecognitionException, Recognizer, Parser}
-import java.util.BitSet
+import java.util.{BitSet, HashSet}
+import scala.collection.mutable
 
-// Define a Scala version of TestError.
 class TestError(msg: String, val line: Int, val column: Int) extends RuntimeException(msg)
 
-// Define a Scala BNFC error listener.
 class BNFCErrorListener extends ANTLRErrorListener {
   override def syntaxError(recognizer: Recognizer[?, ?],
                            offendingSymbol: Object,
@@ -45,29 +45,28 @@ class BNFCErrorListener extends ANTLRErrorListener {
 }
 
 object Main {
+  private val loadedModules = mutable.Set[String]()
+  private val asts = mutable.ListBuffer[Module]()
+
   def main(args: Array[String]): Unit = {
     try {
-      // Use standard input if no file is specified.
-      val input: Reader = if (args.isEmpty) new InputStreamReader(System.in) else new FileReader(args(0))
+      val entryFileOpt = if (args.isEmpty) None else Some(new File(args(0)))
+      val input: Reader = entryFileOpt match {
+        case Some(file) => new FileReader(file)
+        case None       => new InputStreamReader(System.in)
+      }
 
-      // Create the lexer and add the error listener.
-      val lexer = new MettaVenusLexer(new ANTLRInputStream(input))
-      lexer.addErrorListener(new BNFCErrorListener)
-
-      // Create the parser and add the error listener.
-      val tokens = new CommonTokenStream(lexer)
-      val parser = new MettaVenusParser(tokens)
-      parser.addErrorListener(new BNFCErrorListener)
-
-      // Call the entry rule. (Test.java uses start_Module().)
-      val pc = parser.start_Module()
-      val ast = pc.result
+      val entryDir = entryFileOpt.map(_.getParentFile.getCanonicalFile).getOrElse(new File(".").getCanonicalFile)
+      val entryPath = entryFileOpt.map(_.getCanonicalPath).getOrElse("<stdin>")
+      parseModule(input, entryPath, entryDir)
 
       println("\nParse Successful!\n")
-      println("[Abstract Syntax]\n")
-      println(PrettyPrinter.show(ast))
-      println("\n[Linearized Tree]\n")
-      println(PrettyPrinter.print(ast))
+      println("[Abstract Syntax Trees]\n")
+      asts.foreach(ast => println(PrettyPrinter.show(ast)))
+
+      println("\n[Linearized Trees]\n")
+      asts.foreach(ast => println(PrettyPrinter.print(ast)))
+
     } catch {
       case e: TestError =>
         System.err.println(s"At line ${e.line}, column ${e.column} :")
@@ -76,6 +75,46 @@ object Main {
       case e: Exception =>
         e.printStackTrace()
         System.exit(1)
+    }
+  }
+
+  def parseModule(input: Reader, path: String, currentDir: File): Unit = {
+    if (loadedModules.contains(path)) return
+    loadedModules += path
+
+    val lexer = new MettaVenusLexer(new ANTLRInputStream(input))
+    lexer.addErrorListener(new BNFCErrorListener)
+
+    val tokens = new CommonTokenStream(lexer)
+    val parser = new MettaVenusParser(tokens)
+    parser.addErrorListener(new BNFCErrorListener)
+
+    val pc = parser.start_Module()
+    val ast = pc.result.asInstanceOf[Module]
+    asts += ast
+
+    import scala.jdk.CollectionConverters._
+    val imports = ast match {
+      case m: metta_venus.Absyn.ModuleImpl => m.listimport_.asScala.toList
+      case _ => Nil
+    }
+
+    imports.foreach { imp =>
+      val importPath: String = imp match {
+        case i: ImportModule => i.string_
+        case i: ImportModuleAs => i.string_
+        case i: ImportFromModule => i.string_
+      }
+
+      val resolvedFile = {
+        val f = new File(importPath)
+        if (f.isAbsolute) f else new File(currentDir, importPath)
+      }
+
+      val canonicalPath = resolvedFile.getCanonicalPath
+      val reader = new FileReader(resolvedFile)
+      val nextDir = resolvedFile.getParentFile.getCanonicalFile
+      parseModule(reader, canonicalPath, nextDir)
     }
   }
 }

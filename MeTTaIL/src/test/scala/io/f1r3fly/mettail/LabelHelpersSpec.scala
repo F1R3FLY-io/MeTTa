959c4908f89aa48a70afa40bff57b6168d7ed25f
SerhiiK-DevBrother <serhii.kr@devbrother.com>
2025-08-18 11:39:59 +0300

    Recalls a check that does not longer fail

--- MeTTaIL/src/test/scala/io/f1r3fly/mettail/LabelHelpersSpec.scala
+++ MeTTaIL/src/test/scala/io/f1r3fly/mettail/LabelHelpersSpec.scala
@@ -15,7 +15,7 @@ class LabelHelpersSpec extends AnyFlatSpec with Matchers {
     val eqImpl = new EquationImpl(ast1, ast2)
 
     val labels = LabelHelpers.labelsInEquation(eqImpl)
-    // fails, but do not commit this change: labels should contain allOf ("foo", "(:){qux}")
+    labels should contain allOf ("foo", "(:){qux}")
     labels.size shouldEqual 2
   }
 

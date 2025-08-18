de86ef11bf775ecf9df67aa28c5fe47b27a013a6
SerhiiK-DevBrother <serhii.kr@devbrother.com>
2025-08-18 11:25:24 +0300

    Recalls a check that does not longer fail

--- MeTTaIL/src/test/scala/io/f1r3fly/mettail/AddEqRwHelpersSpec.scala
+++ MeTTaIL/src/test/scala/io/f1r3fly/mettail/AddEqRwHelpersSpec.scala
@@ -66,7 +66,7 @@ class AddEqRwHelpersSpec extends AnyFunSuite {
     val right = COAVar("v")
     val err = sameCategory(left, right, pretty)
     assert(err.isLeft)
-    // fails: assert(err.left.get.contains("Label L not found in structure"))
+    assert(err.left.get.contains("Label L not found in structure"))
   }
 
   test("sameCategory should error when both sides are variables") {
